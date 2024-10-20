88650b628d398c8dcb516eb2577307b77433b71f
Sina Ghazi <sinaghazi@gmail.com>
2024-10-20 22:57:55 +0300

    Update selection buttons styling and behavior

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,21 @@ module.exports = {
       colors: {
         'primary': '#ff3300',
         'secondary': '#FDE1C4',
+        'dark-gray': '#333333',
+      },
+      fontFamily: {
+        'sans': ['Open Sans', 'Arial', 'sans-serif'],
+      },
+      fontSize: {
+        'custom-size': '18px',
+        '2.5xl': '1.75rem',  // This is between 2xl and 3xl
+        '3.5xl': '2rem',     // This is between 3xl and 4xl
+      },
+      lineHeight: {
+        'custom-leading': '1.6',
+      },
+      backgroundColor: {
+        'secondary-60': 'rgba(253, 225, 196, 0.6)',
       },
     },
   },
